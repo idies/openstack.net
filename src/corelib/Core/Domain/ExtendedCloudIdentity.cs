207bcc4c33d3fefaae515fe06da4df3e8671dba5
dmedv <dmedv@pha.jhu.edu>
2014-04-30 15:48:19 -0400

    Additional identity field - TenantName

---
+++ src/corelib/Core/Domain/ExtendedCloudIdentity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace net.openstack.Core.Domain
+{
+    public class ExtendedCloudIdentity : CloudIdentity
+    {
+        public string TenantName { get; set; }
+    }
+}
